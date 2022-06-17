7a73d4b4291570bf241f94db508778f577583d07
Fullstack Ruby On Rails Dev <vshwsnahar3@gmail.com>
2022-06-17 14:47:30 +0530

    Create solution-03.rb

---
+++ solution-03.rb
@@ -0,0 +1,15 @@
+num = 0
+num_next = 1
+def fibonnaci(count)
+    (count - 2).times do
+      num = num + num_next
+      num_next = num + num_next
+      puts a
+    end
+    num_next
+end
+
+fibonnaci(0)
+fibonnaci(1)
+fibonnaci(4)
+fibonacci(7)
